6cab6b88370a0470958b9fe37c28019f5238eb55
Alexandre Hannebelle <alexhannebelle@gmail.com>
2020-12-08 22:11:20 +0100

    Fix paths and change layout

--- src/models/messenger.py
+++ src/models/messenger.py
@@ -10,6 +10,7 @@ from src.models import BaseChatAppModel
 MESSENGER_PHOTOS_FOLDER = "../data/messenger/photos"
 MESSENGER_GIFS_FOLDER = "../data/messenger/gifs_as_frame"
 MESSENGER_VIDEOS_FOLDER = "../data/messenger/videos_as_frame"
+EMOJI_TYPE = 'android'
 
 
 class MessengerModel(BaseChatAppModel):
@@ -36,7 +37,7 @@ class MessengerModel(BaseChatAppModel):
         #raw_data_messenger = pd.read_json('messenger/inbox/marcnegre_hwizlpvhxw/message_1.json', lines=True)
         # raw_data_messenger.info()
         # raw_data_messenger.head()
-        for message in raw_data["messages"][0:900000000]:  # only 100 messages to test
+        for message in raw_data["messages"][0:100000]:  # only 100 messages to test
 
             timestamp = message["timestamp_ms"] / 1000
             timestamp = datetime.datetime.fromtimestamp(
@@ -83,7 +84,7 @@ class MessengerModel(BaseChatAppModel):
                 text = text.replace(url, "\\texttt{"+url[1:30]+"...}")
             # deal with emoji
                 # TODO: fix this undefined emoji: "not(c in emoji.UNICODE_EMOJI):"
-            new_text = "".join(f"\emoji[ios]{{{ord(c):X}}}" if c in emoji.UNICODE_EMOJI else c for c in text) 
+            new_text = "".join(f"\emoji[{EMOJI_TYPE}]{{{ord(c):X}}}" if c in emoji.UNICODE_EMOJI else c for c in text) 
 
             # new_text = ""
             # for c in text:
@@ -93,7 +94,7 @@ class MessengerModel(BaseChatAppModel):
             #     else:
             #         # Change for f string instead of [2:]
             #         new_text = new_text + \
-            #             "\emoji[ios]{"+f"{hex(ord(c))[2:]}"+"}"
+            #             "\emoji[{EMOJI_TYPE}]{"+f"{hex(ord(c))[2:]}"+"}"
 
             if message.get('photos') is not None:
                 photo = []
@@ -128,7 +129,7 @@ class MessengerModel(BaseChatAppModel):
             if message.get('reactions') is not None:
             	react = (message["reactions"][0]["reaction"]).encode('latin-1').decode('raw_unicode_escape').encode(
                     'latin-1').decode('utf-8')
-            	new_react = "".join(f"\emoji[ios]{{{ord(c):X}}}" if c in emoji.UNICODE_EMOJI else c for c in react)
+            	new_react = "".join(f"\emoji[{EMOJI_TYPE}]{{{ord(c):X}}}" if c in emoji.UNICODE_EMOJI else c for c in react)
             	reactions.append(new_react)
             	
 
@@ -153,23 +154,16 @@ class MessengerModel(BaseChatAppModel):
         return file_location
 
     def _reformat_gifs_path(self, uri):
-        #print(uri, re.search("([0-9]*)_([0-9]*)_([0-9]*)_(.).[(jpg)|(png)]", uri))
-        #res = re.search("([0-9]*)_([0-9]*)_([0-9]*)_(.).[(jpg)|(png)]", uri)
         res = re.search("([0-9]*)_([0-9]*)_([0-9]*)_(.).", uri)
-        #file_name = f"{res.group(1)}_{res.group(2)}_{res.group(3)}_{res.group(4)}_{res.group(2)}.png"
         file_name = f"{res.group(1)}_{res.group(2)}_{res.group(3)}_{res.group(4)}.png"
         file_location = os.path.join(MESSENGER_GIFS_FOLDER, file_name)
         return file_location
 
     def _reformat_image_path(self, uri):
-        #print(uri, re.search("([0-9]*)_([0-9]*)_([0-9]*)_(.).[(jpg)|(png)]", uri))
-        #res = re.search("([0-9]*)_([0-9]*)_([0-9]*)_(.).[(jpg)|(png)]", uri)
         res = re.search("([0-9]*)_([0-9]*)_([0-9]*)_(.).", uri)
         if 'png' in uri:
-            #file_name = f"{res.group(1)}_{res.group(2)}_{res.group(3)}_{res.group(4)}_{res.group(2)}.png"
             file_name = f"{res.group(1)}_{res.group(2)}_{res.group(3)}_{res.group(4)}.png"
         else:
-            #file_name = f"{res.group(1)}_{res.group(2)}_{res.group(3)}_{res.group(4)}_{res.group(2)}.jpg"
             file_name = f"{res.group(1)}_{res.group(2)}_{res.group(3)}_{res.group(4)}.jpg"
         file_location = os.path.join(MESSENGER_PHOTOS_FOLDER, file_name)
         return file_location
